bc149a69d1bbf328f3197d950d9c37ecb1723e2a
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-26 17:26:29 +0300

    change syntax

--- photos/urls.py
+++ photos/urls.py
@@ -1,6 +1,9 @@
 from django.urls import re_path
 from . import views
 
-urlpatterns = {
+
+urlpatterns = [
     re_path ('^$',views.my_Photo,name='myPhoto'),
-}
+    re_path ('^today/$',views.news_of_day,name='newsToday'),
+]
+   
