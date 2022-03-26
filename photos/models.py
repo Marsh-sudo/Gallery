d8e66e09c6d21a2a038f57a101bf0edaf5a41693
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-26 15:23:03 +0300

    create class category

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,19 @@
+from unicodedata import category
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Location(models.Model):
+    location_name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.location_name
+
+    def save_location(self):
+        return self.save()
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.category_name
