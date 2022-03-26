af0d1f6ebc401478d0031869a9000a16587e1e45
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-27 01:19:56 +0300

    add message property to function search

--- photos/views.py
+++ photos/views.py
@@ -1,12 +1,28 @@
+from unicodedata import category
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime as dt
+from .models import Image, Location
 
 
 
 
 # Create your views here.
 def my_Photo(request):
-    return render(request ,'welcome.html')
+    photo  = Image.show_all_images()
+    locations = Location.objects.all()
+    return render(request, 'all-photos/photo.html',{"photo":photo,"location":locations})
 
 
+def search_results(request):
+    if 'search' in request.GET and request.GET["search"]:
+        category = request.GET.get("search")
+        searched_image = Image.search_image_by_category(category)
+        locations = Location.objects.all()
+        message = f"{category}"
+
+        return render (request,'all-photos/search.html',{"message":message,"images":searched_image,"location":locations})
+
+    else:
+        message = "You haven't searched for any term"
+        return render (request, 'all-photos/search.html',{"message":message})
