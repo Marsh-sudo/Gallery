a1506e463276e61a96d904a0f56e382f5eebf887
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-26 18:11:20 +0300

    create test for save method

--- photos/tests.py
+++ photos/tests.py
@@ -28,3 +28,24 @@ class CategoryTestClass(TestCase):
     def setUp(self):
         self.category = Category(category_name = 'travel')
         self.category.save()
+
+
+class ImageTestClass(TestCase):
+
+    def setUp(self):
+        #creating a new location saving it
+        self.London = Location(location_name = 'London')
+        self.London.save_location()
+
+        #creating a new category
+        self.new_category = Category(category_name = 'travel')
+        self.new_category.save_category()
+
+        self.new_image = Image(img_name = 'The mountains',img_desc = 'The mountains of Iceland',location = self.London)
+        self.new_image.save()
+
+    #test save method
+    def test_save_method(self):
+        self.new_image.save_image()
+        image = Image.objects.all()
+        self.assertTrue(len(image) > 0)
