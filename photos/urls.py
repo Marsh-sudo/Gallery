c6124770f613b1846dc3f3ba4f14e760ac02d42b
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-27 01:19:03 +0300

    add debug settings for media

--- photos/urls.py
+++ photos/urls.py
@@ -1,9 +1,14 @@
 from django.urls import re_path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 
 urlpatterns = [
     re_path ('^$',views.my_Photo,name='myPhoto'),
-    re_path ('^today/$',views.news_of_day,name='newsToday'),
+    re_path ('^search/',views.search_results,name = 'search_results')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+
    
